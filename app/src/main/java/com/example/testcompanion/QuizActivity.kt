e91fcf438ff3b2a074f635a65767cf74330c8077
JrAppDeveloper <jrappdeveloper.cdg@gmail.com>
2024-02-18 19:33:05 +0500

    Added Shimmer Effect and Countdown Timer Functionality

--- app/src/main/java/com/example/testcompanion/QuizActivity.kt
+++ app/src/main/java/com/example/testcompanion/QuizActivity.kt
@@ -2,6 +2,7 @@ package com.example.testcompanion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,11 +10,13 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.testcompanion.databinding.ActivityQuizBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firestore.v1.StructuredAggregationQuery.Aggregation.Count
 
 class QuizActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQuizBinding
     private lateinit var quizAdapter: QuizAdapter
+    private lateinit var countDownTimer: CountDownTimer
     private val quizQuestions = ArrayList<QuizQuestion>()
     private var currentItemPosition = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,7 +61,7 @@ class QuizActivity : AppCompatActivity() {
         if (Constant.PrepareMode){
             binding.tagMode.text = "Prepare"
         }else{
-            binding.tagMode.text = "Quiz"
+            binding.tagMode.text = "00:00"
         }
 
     }
@@ -85,6 +88,23 @@ class QuizActivity : AppCompatActivity() {
 
                 // Notify the adapter that data has changed
                 quizAdapter.notifyDataSetChanged()
+                binding.shimmerFrameLayout.visibility = View.GONE
+                binding.viewPager.visibility = View.VISIBLE
+                if (!Constant.PrepareMode){
+                    countDownTimer = object : CountDownTimer(2400000, 1000){ // 40 minutes in milliseconds
+                        override fun onTick(millisUntilFinished: Long) {
+                            //remaining time in minutes and milliseconds
+                            val minutes = millisUntilFinished / 60000
+                            val seconds = (millisUntilFinished % 60000) / 1000
+                            binding.tagMode.text = String.format("%02d:%02d", minutes, seconds)
+                        }
+
+                        override fun onFinish() {
+                            TODO("Not yet implemented")
+                        }
+
+                    }.start()
+                }
             }
             .addOnFailureListener { exception ->
                 // Handle errors here
@@ -95,6 +115,14 @@ class QuizActivity : AppCompatActivity() {
         super.onBackPressed()
         Constant.selectedOptions.clear()
         Constant.universalIndex = 0
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (!Constant.PrepareMode){
+            countDownTimer.cancel()
+        }
+
     }
 
 }
